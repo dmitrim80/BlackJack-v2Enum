91fa9ed41e6af4661e41782ed9bb7037bdf1098a
Dmitri Morozov <dmitri.m80@gmail.com>
2023-07-03 14:48:50 -0400

    Initial Commit

---
+++ BlackJack-v2Enum/BlackJack_v2EnumApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJack_v2EnumApp.swift
+//  BlackJack-v2Enum
+//
+//  Created by Dmitri Morozov on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BlackJack_v2EnumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackJack-v2EnumUITests/BlackJack_v2EnumUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BlackJack_v2EnumUITestsLaunchTests.swift
+//  BlackJack-v2EnumUITests
+//
+//  Created by Dmitri Morozov on 7/3/23.
+//
+
+import XCTest
+
+final class BlackJack_v2EnumUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
